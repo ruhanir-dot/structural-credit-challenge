bc549d36c0f29adea64292151fcc26c74199adf5
ruhanir-dot <ruhanirekhi@gmail.com>
2025-12-21 17:01:11 -0800

    risk_measures
    move to main execution

--- naive_model/risk_measures.py
+++ naive_model/risk_measures.py
@@ -38,9 +38,19 @@ def distance_to_default(V, D, T, r, sigma_V):
     # DD = (E[V_T] - D) / std(V_T)
     # where E[V_T] = V * exp(r*T)
     # and std(V_T) = V * exp(r*T) * sqrt(exp(sigma_V^2*T) - 1)
-    
-    raise NotImplementedError("Implement distance-to-default calculation")
 
+    if V <= 0 or sigma_V <= 0 or T<= 0 or D <= 0:
+        return np.nan
+    
+    expected_V_T = V * np.exp(r *T)
+    std_V_T = V* np.exp(r * T) * np.sqrt(np.exp((sigma_V ** 2) * T) - 1)
+
+    if std_V_T == 0: 
+        return np.nan
+    
+    d_to_d = (expected_V_T - D)/ std_V_T
+
+    return d_to_d
 
 def default_probability(V, D, T, r, sigma_V):
     """
@@ -70,8 +80,11 @@ def default_probability(V, D, T, r, sigma_V):
     # Hint: See Mathematical Background section in README.md
     # PD = Phi(-d2) where d2 = (ln(V/D) + (r - sigma_V^2/2)*T) / (sigma_V*sqrt(T))
     
-    raise NotImplementedError("Implement default probability calculation")
+    d2 = (np.log(V/D) + (r - (sigma_V**2)/2) * T) / (sigma_V * np.sqrt(T))
 
+    default_prob = norm.cdf(-d2)    
+
+    return default_prob
 
 def compute_risk_measures(V, D, T, r, sigma_V):
     """
@@ -102,4 +115,3 @@ def compute_risk_measures(V, D, T, r, sigma_V):
         'DD': DD,
         'PD': PD
     }
-
